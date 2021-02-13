d203abec59bda218c3024086277005ef50c80228
Mayte <mayte.valdez.abad@gmail.com>
2021-02-13 05:50:10 +0100

    nav + structure

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
 import { Link } from 'react-router-dom';
+import Avatar from '../images/Avatar.svg';
+import Logo from '../images/Logo.svg';
+import '../css/NavBar.css'
 
 function NavBar() {
     return (
-        <Navbar bg="light" variant="light">
-           <Navbar.Brand >
+        <Navbar className="nav" collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" /> 
+          <Navbar.Brand className="logo">
                <Link to={'/'}><img
-                   alt=""
-                   src="/logo.svg"
-                   width="30"
+                   alt="logo TroopShow"
+                   src={Logo}
+                   width="auto"
                    height="30"
                    className="d-inline-block align-top"
-               />{' '}
-               TroopShow</Link>
+               />{' '}</Link>
            </Navbar.Brand>
-           <Navbar.Collapse id="basic-navbar-nav">
+           
+           <Navbar.Collapse id="responsive-navbar-nav">
                <Nav className="mr-auto">
                    <Nav.Item><Link to={'/donation'}>Donation</Link></Nav.Item>
                    <Nav.Item><Link to={'/about'}>About</Link></Nav.Item>
                    <Nav.Item><Link to={'/contact'}>Contact</Link></Nav.Item>
                </Nav>
            </Navbar.Collapse>
-           <Button variant="outline-primary">Log in</Button>
-          
+           <Nav.Item className="avatar"><Link to={'/conexion'}><img  src={Avatar}/></Link></Nav.Item>
        </Navbar>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import NavBar from '../components/NavBar.js';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 
 function Home() {
     return (
-        <>
+        <div>
             <NavBar/>
-            <h1>The Show must go on</h1>
-
-        </>
+            <Container>
+                <h1>The Show must go on</h1>
+            </Container>
+        </div>
     )
 
 };
